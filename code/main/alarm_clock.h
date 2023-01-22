28806bba256eb35178b68e65d20711b53e031fa6
Mattis Jaksch <Mattis.Jaksch@dlr.de>
2023-01-22 11:33:44 +0100

    Added time acquisition

--- code/main/alarm_clock.h
+++ code/main/alarm_clock.h
@@ -8,6 +8,22 @@
  
 #pragma once
 
+#define CONFIG_SNTP_TIME_SERVER "pool.ntp.org"
+
+// TODO Make this time time variable maybe (?)
+/* Variable holding number of times ESP32 restarted since first boot.
+ * It is placed into RTC memory using RTC_DATA_ATTR and
+ * maintains its value when ESP32 wakes from deep sleep.
+ */
+ 
+//RTC_DATA_ATTR static int boot_count = 0;
+
+void obtain_time();
+void initialize_sntp();
+
+
+void test_alarm();
+
  // Timer construct
  
  // Time light correlation array
